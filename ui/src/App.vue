38e459d8d59788b818eaec552123191a993171a1
Robinson Santos - Engenharia - Inobram Automações <robinsonsantos@localhost.localdomain>
2020-12-11 17:38:15 -0300

    ui - using only vuejs

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- <div id="nav">
+    <router-link to="/signin">Signin</router-link> |
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
   }
-}
-</script>
+
+  a {
+    text-decoration: none;
+  }
+
+  html, body, #app {
+    height: 100%;
+    width: 100%;
+  }
+
+  #app {
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
---
+++ ui/src/views/Signin.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="box">
+      <form @submit.prevent="logIn">
+        <input type="text">
+        <input type="text">
+        <button type="submit"></button>      
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logIn: function () {
+      console.log('log in')
+    }
+  }  
+}
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  background:grey;
+  height: 100vh;
+}
+
+.box {
+  margin: auto;
+}
+
+</style>
