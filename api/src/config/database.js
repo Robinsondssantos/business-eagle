b5a140cb4ed9abfea5286ee3ea25e0c0cec3a3c9
Robinson Santos - Engenharia - Inobram Automações <robinsonsantos@localhost.localdomain>
2020-12-16 12:57:43 -0300

    api - fix database config

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -5,7 +5,7 @@ module.exports = {
     logging: true,
     database: process.env.NODE_ENV === 'production' ? process.env.DB_NAME : 'business_eagle',
     username: process.env.NODE_ENV === 'production' ? process.env.DB_USERNAME : 'postgres',
-    password: Process.env.NODE_ENV === 'production' ? process.env.DB_PASSWORD : 'postgres',
+    password: process.env.NODE_ENV === 'production' ? process.env.DB_PASSWORD : 'postgres',
     define: {
       timestamps: true,
       underscored: true,
